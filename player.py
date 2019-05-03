5fe64bf7e7a6bf604a697a3cb6768b28c07d9b13
Jake Faulkner <jakefaulkn@gmail.com>
2019-05-03 16:44:25 +1200

    feat: add extra player helper functions

--- player.py
+++ player.py
@@ -25,6 +25,35 @@ def player_inflict_damage(player, monster):
     return (hp - damage, hear, m_damage)
 
 
+def player_position(player):
+    """ Return the player's current position.
+
+    Examples:
+    >>> player_position(new_player(10, 10))
+    (10, 10) """
+    x, y, _, _ = player
+    return x, y
+
+
+def player_hp(player):
+    """ Return the player's current hp.
+
+    Examples:
+    >>> player_hp(new_player(0, 0))
+    10
+     """
+    return player[2]
+
+
+def player_damage(player):
+    """ Return the player's current damage.
+
+    Examples:
+    >>> player_damage(new_player())
+    1 """
+    return player[3]
+
+
 def is_dead(player):
     _, _, hp, _ = player
     return hp <= 0
